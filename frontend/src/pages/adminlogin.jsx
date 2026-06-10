e226f8f30a3c87a538caa23830678c560fe02d5b
Minakshi yadav <yadavminakshi864@gmail.com>
2026-06-10 12:36:24 +0530

    Fix import path for auth store in adminlogin.jsx

--- frontend/src/pages/adminlogin.jsx
+++ frontend/src/pages/adminlogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import useAuthStore from '../store/authStore'
+import useAuthStore from '../store/authstore'
 import api from '../api'
 
 const s = {
